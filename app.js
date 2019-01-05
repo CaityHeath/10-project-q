1bfdad3897b06a3a789268f8ee191649a656ef00
LENA EIVY <applena@gmail.com>
2019-01-05 13:46:00 -0800

    update

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 'use strict';
 
 const Q = require('./lib/server.js');
+const config = require('./config');
+
+//start the default socket listener
 Q.start();
 
-const db = new Q('database');
-db.monitorEvent('create');
-db.monitorEvent('update');
-db.monitorEvent('delete');
+//add all listeners and monitors for namespaces
+for (let name in config.queues){
+  let queue = new Q(name);
+  config.queues[name].forEach(room => {
+    queue.monitorEvent(room);
+  });
+}
 
-const network = new Q('network');
-network.monitorEvent('attack');
-network.monitorEvent('no-service');
+
