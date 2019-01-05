322683e7a543091426b96b377912c1de4a89c053
LENA EIVY <applena@gmail.com>
2019-01-05 12:36:25 -0800

    putting files together

---
+++ clients/subscriberClient.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const Q = require('../lib/subscriber.js');
+
+const db = new Q('database');
+
+db.subscribe('delete', (payload) => {
+  console.log('delete happened', payload);
+});
+
+db.subscribe('create', (payload) => {
+  console.log('create happened', payload);
+});
+
+console.log(db.subscriptions());
---
+++ config/index.js
@@ -0,0 +1,7 @@
+// json structure like we would eventually get from a mongodb
+module.exports = {
+  queues: {
+    database: ['create', 'update', 'delete'],
+    network: ['attack', 'no-service'],
+  },
+};
