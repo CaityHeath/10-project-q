8d365748cfb480e4df0c4c1df6165c040c90d9a6
LENA EIVY <applena@gmail.com>
2019-02-15 07:43:23 -0800

    server is up

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 'use strict';
 
 const Q = require('./lib/server.js');
-const config = require('./config');
+//const config = require('./config');
 console.log('hello from app.js');
 
 //start the default socket listener
-Q.start();
+// Q.start();
+
+// const chatQueue = new Q('troll');
+// chatQueue.monitorEvent('message');
+// chatQueue.monitorEvent('history');
 
 //add all listeners and monitors for namespaces
-for (let name in config.queues){
-  new Q(name);
-}
+// for (let name in config.queues){
+//   new Q(name);
+// }
 
-//Q.stop();
+Q.start();
+const database = new Q('database');
+database.monitorEvent('update');
+database.monitorEvent('create');
+database.monitorEvent('read');
+database.monitorEvent('delete');
 
 
